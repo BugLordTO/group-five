93278817aa8057ddeca0f3330ec545acd351a9aa
Somkiet <somsor@perfenterprise.com>
2018-09-22 11:25:26 +0700

    init project

---
+++ group-five/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace group_five.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
